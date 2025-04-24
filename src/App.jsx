682366adf6433fdf5670db2c5ce47a2addfcf49b
Aditya kumar <ak6101283@gmail.com>
2025-04-24 12:41:43 +0530

    making this digital clock more performent

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [clock, setClock] = useState(0)
+
+  let date = new Date();
+
+  useEffect(()=>{
+    setInterval(()=>{
+      setClock((p) => p+1 )
+    },1000)
+  },[])
+
+  function getdate(){
+    let result = '';
+
+    const date = new Date();
+
+    let hr = date.getHours();
+
+    const mm = date.getMinutes().toString().padStart(2,"0");
+
+    const sec = date.getSeconds().toString().padStart(2,"0");
+     
+    let ampm = hr >12 ? "PM": "AM";
+
+    hr = hr % 12 || 12;
+      
+
+    return `${hr}:${mm}:${sec} ${ampm}`;
+
+  }
 
   return (
     <>
+
       <h1>Digital Clock </h1>
+
+      {// date.toLocaleTimeString()
+      }
+
+      {getdate()}
         
        <div></div>
         
